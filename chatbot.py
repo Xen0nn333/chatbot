f9440a22d9e4bffde833a29af948407fabf1c32b
Xen0nn333 <153431970+Xen0nn333@users.noreply.github.com>
2024-01-13 23:55:33 +0200

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,7 +1,10 @@
 import discord
 from discord.ext import commands
+import random
+import asyncio
 
 intents = discord.Intents.default()
+intents.members = True
 intents.message_content = True
 
 bot = commands.Bot(command_prefix='$', intents=intents)
@@ -18,4 +21,37 @@ async def hello(ctx):
 async def heh(ctx, count_heh = 5):
     await ctx.send("he" * count_heh)
 
-bot.run("OTE4ODQ1MTQ2MTk4NTgxMjc4.GhyqJW.4Gtvi_BcYXmfXBJhUEqiNvcCK1wPowPf0ANYSU")
+@bot.command()
+async def repeat(ctx, times: int, content='repeating...'):
+    for i in range(times):
+        await ctx.send(content)
+
+@bot.command()
+async def joined(ctx, member: discord.Member):
+    await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')
+
+    
+    async def on_message(self, message):
+        # we do not want the bot to reply to itself
+        if message.author.id == self.user.id:
+            return
+
+        if message.content.startswith('$guess'):
+            await message.channel.send('Угадай число с 1 до 10.')
+
+            def is_correct(m):
+                return m.author == message.author and m.content.isdigit()
+
+            answer = random.randint(1, 10)
+
+            try:
+                guess = await self.wait_for('message', check=is_correct, timeout=5.0)
+            except asyncio.TimeoutError:
+                return await message.channel.send(f'Извини, ты не успел написать правильный ответ. Ответ был {answer}.')
+
+            if int(guess.content) == answer:
+                await message.channel.send('Правильно!')
+            else:
+                await message.channel.send(f'Неправильно. На самом деле это {answer}.')
+
+bot.run("token")
